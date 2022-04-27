65dae582e4460254d929dc99be355488207b362c
Juan Pablo Cadena <juanpablocadena@MacBook-Air-de-Juan-2.local>
2022-04-26 19:31:44 -0500

    [ADD] redux && poke api

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URL = process.env.POKEAPI ;
+const URL = process.env.REACT_APP_POKEAPI || 'https://pokeapi.co/api/v2/' ;
+console.log(process.env.REACT_APP_POKEAPI);
 const axiosInstance = axios.create({
     baseURL:URL
 });
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,23 @@
 import React, {useEffect} from 'react';
+import { List } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 import { getPokemons } from '../../api/getPokemons';
+import { setPokemon } from '../../actions';
 import PokemonList from '../../components/PokemonList';
 import Searcher from '../../components/Searcher';
 import './styles.css';
 
-function Home() {
+const mapStateToProps = state =>({
+  list: state.list
+})
+
+const mapDispatchToProps = dispatch => ({
+  setPokemons: value => dispatch(setPokemon(value)) 
+})
+function Home({list, setPokemons}) {
   useEffect(() =>{
-    getPokemons().then((res) => console.log(res));
-  })
+    getPokemons().then((res) => setPokemons(res.data.results));
+  },[])
   return (
     <div className='Home'>
       <Searcher />
@@ -16,4 +26,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
