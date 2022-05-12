c76e77c40cae13ecab0c93d69ced0bd8dba4280c
Juan Pablo Cadena <juanpablocadena@MacBook-Air-de-Juan-2.local>
2022-05-12 12:42:30 -0500

    [ADD] pokemon card with api data

--- src/components/PokemonList/PokemonCard.js
+++ src/components/PokemonList/PokemonCard.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Label, Image, Grid, Icon } from 'semantic-ui-react';
 import { MAIN_COLOR, FAV_COLOR } from "../../utils/constants";
 
-const Pokemoncard = ({pokemon}) => {
+const PokemonCard = ({pokemon}) => {
     return (
         <Grid.Column mobile ={16} tablet={8} computer={4}>
             <div className='PokemonCard'>
                 <Icon name='favorite' color={FAV_COLOR} />
-                <Image centered src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' alt='Pokemon Front' />
+                <Image centered src={pokemon.sprites.front_default} alt='Pokemon Front' />
                 <p className='PokemonCard-title'>{pokemon.name}</p>
-                <Label color={MAIN_COLOR}>Normal</Label>
+                {pokemon.types && pokemon.types.map(types => 
+                    <Label color={MAIN_COLOR} key={`${pokemon.id}_${types.type.name}`}>{types.type.name}</Label>
+                )}
             </div>
         </Grid.Column>
     );
 }
 
-export default Pokemoncard;
+export default PokemonCard;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -7,13 +7,18 @@ import { setError } from '../../actions';
 import PokemonList from '../../components/PokemonList';
 import Searcher from '../../components/Searcher';
 import './styles.css';
+import axios from 'axios';
 
 function Home() {
   const dispatch = useDispatch();
   const list = useSelector(state => state.list);
   useEffect(() =>{
     getPokemons().then((res) => {
-      dispatch(setPokemon(res.data.results))
+      const pokemonList = res.data.results;
+      return Promise.all(pokemonList.map(pokemon => axios.get(pokemon.url)));
+    }).then((pokemonResponse )=>{
+      const pokemonsData = pokemonResponse.map( response => response.data);
+      dispatch(setPokemon(pokemonsData));
     }).catch((error) => {
       dispatch(setError({ message: 'Ocurrió un error' , error}));
     });
